4f2672b9bd09ddaa4a649b86032acd0e47678db8
unfrgivn <bradley.ash@gmail.com>
2020-03-30 23:55:56 -0500

    Set starting point for changelog based on when package.json versions existed

--- .release-it.js
+++ .release-it.js
@@ -5,7 +5,8 @@ module.exports = {
         'after:release': 'echo Successfully released ${name} v${version} to ${repo.repository}.',
     },
     git: {
-        changelog: 'npx auto-changelog -p --commit-limit false --stdout --unreleased --template tpl-release-changes.hbs',
+        changelog:
+            'npx auto-changelog -p --commit-limit false --stdout --unreleased --template tpl-release-changes.hbs --starting-commit 7fe03008f2c6f39f3475808cf6416da008d32fc2',
         commitMessage: 'chore(release): Publish v${version}',
         requireUpstream: false,
         tagName: 'v${version}',
