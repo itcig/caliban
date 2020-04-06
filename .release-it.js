0dd792d835557d7d5b6232d2d8f4e9abaf1dd719
unfrgivn <bradley.ash@gmail.com>
2020-04-05 22:48:32 -0500

    Updated auto-changelog config

--- .release-it.js
+++ .release-it.js
@@ -5,8 +5,7 @@ module.exports = {
         'after:release': 'echo Successfully released ${name} v${version} to ${repo.repository}.',
     },
     git: {
-        changelog:
-            'npx auto-changelog -p --commit-limit false --stdout --unreleased --template tpl-release-changes.hbs --starting-commit 7fe03008f2c6f39f3475808cf6416da008d32fc2',
+        changelog: 'npx auto-changelog -p --commit-limit false --stdout --unreleased --template tpl-release-changes.hbs',
         commitMessage: 'chore(release): Publish v${version}',
         requireUpstream: false,
         tagName: 'v${version}',
